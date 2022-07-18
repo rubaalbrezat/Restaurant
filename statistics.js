319aafdc374a2e6bf65ee04264e3cce484be1d84
rubaalbrezat <rubaalbrezat@gmail.com>
2022-07-18 21:21:37 +0300

    aaaaa

--- statistics.js
+++ statistics.js
@@ -1,21 +1,50 @@
-function saveData(){
-    let stringifyedData = JSON.stringify(foodArr)
-    localStorage.setItem("food",stringifyedData)
-  
-};
+
+'use strict';
 
 
 
+function get1() {
+    let getArr = localStorage.getItem("saveKey");
+    let newArr = JSON.parse(getArr);
 
-function getData(){
-    let retrievedData = localStorage.getItem("food");
-    let parsedData = JSON.parse(retrievedData);
-    if (parsedData != null){
-    for (i = 0; i < parsedData.length; i++){
-        newType = new Food (parsedData[i].foodID,parsedData[i].foodName,parsedData[i].type,parsedData[i].price);
-        newType.tableRender();
-    }
    
+     if(newArr != null){
+    for (let i = 0 ; i < newArr.length ; i++) {
+
+        let tablePlace = document.getElementById("table");
+
+        let tableRow = document.createElement("tr");
+        let tableData1st = document.createElement("td");
+        let tableData2nd = document.createElement("td");
+        let tableData3rd = document.createElement("td");
+        let tableData4th = document.createElement("td");
+
+        tableData1st.textContent = newArr[i]["foodID"];
+        tableData2nd.textContent = newArr[i]["foodName"];
+        tableData3rd.textContent = newArr[i]["foodtype"];
+        tableData4th.textContent = newArr[i]["foodprice"];
+
+        tableRow.appendChild(tableData1st);
+        tableRow.appendChild(tableData2nd);
+        tableRow.appendChild(tableData3rd);
+        tableRow.appendChild(tableData4th);
+        tablePlace.appendChild(tableRow);
+
+
+
+
+    }
+
+
+
 }
+
+
+
+
+
 }
-getData();
+get1();
+
+
+
