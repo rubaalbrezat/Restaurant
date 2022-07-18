d8b8a56d48df6e0c2bfba9f74659bad59c87f9fe
rubaalbrezat <rubaalbrezat@gmail.com>
2022-07-18 03:59:22 +0300

    aaa

--- app.js
+++ app.js
@@ -1,71 +1,147 @@
-`use strict`
+/*`use strict`
+let newFood1 = document.getElementById("formFood");
 
 
-    const allFood =[];
-    
-    function Food(FoodID,FoodName,FoodType,FoodPrice)
-    {
-  this.FoodID = FoodID;
-  this.FoodName = FoodName;
-  this.FoodType = FoodType;
-  this.FoodPrice = FoodPrice; 
+const allFood = [];
+
+let a = 0
+function Food(FoodID,FoodName, FoodType, FoodPrice) {
+   this.FoodID = a++;
+   this.FoodName = FoodName;
+   this.FoodType = FoodType;
+   this.FoodPrice = FoodPrice;
    allFood.push(this);
+
+
+}
+
+
+ let s = new Food(FoodID,FoodName,FoodType,FoodPrice);
+ s.render();
+
+
+newFood1.addEventListener("submit", handleSubmit);
+
+
+
+function handleSubmit(event) {
+   event.preventDefult();
+
+   let FoodName = event.targert.FoodName.value;
+   let FoodType = event.targert.FoodType.value;
+   let FoodPrice = event.targert.FoodPrice.value;
+
+
+   let newFood = new Food(FoodName, FoodType, FoodPrice);
+   newFood.render();
+
+}
+
+
+
+
+
+
+
+/*Food.prototype.render = function () {
+   let trE = document.createElement("tr");
+
+   trE.innerHTML = `
+   <td>${this.FoodID}</td>
+   <td>${this.FoodName}</td>
+   <td>${this.FoodType}</td>
+   <td>${this.FoodPrice}</td>
+   
+   
+   
+  `;
+
+   newTable.appendChild(trE);
+
+
+}
+
+Food.prototype.render = function () {
+   /*let bd = document.getElementById("body");
+   let trE = document.createElement("tr");
+   let tdata1 = document.createElement("td");
+   let tdata2 = document.createElement("td");
+   let tdata3 = document.createElement("td");
+   let tdata4 = document.createElement("td");
+   tdata1.textContent=this.FoodID;
+   tdata2.textContent=this.FoodName;
+   tdata3.textContent=this.FoodType;
+   tdata4.textContent=this.FoodPrice;
+   trE.appendChild(tdata1);
+   trE.appendChild(tdata2);
+   trE.appendChild(tdata3);
+   trE.appendChild(tdata4);
+   
+   bd.appendChild(trE);
+
+   let q= document.getElementById("mn");
+   let b =document.createElement("p");
+   b.textContent = this.FoodName;
+    q.appendChild(b);
+
+
    
 
+
+
+
+
+}*/
+
+'use strict';
+
+function Food (foodName,type,price) {
+    
+    this.foodID= this.randomIDNumber();
+    this.foodName=foodName;
+    this.type=type;
+    this.price=price;
+
+    console.log (this);
+}
+
+Food.prototype.randomIDNumber= function() {
+      return (Math.floor(Math.random()*(9999-1000+1))+1000);
     }
 
 
-   let newFood1 =document.getElementById("formFood "); 
+Food.prototype.render = function() {
 
-    newFood.addEventListener("submit",handleSubmit) ;
+    let tablePlace = document.getElementById("jsTableID");
 
+    let tableRow = document.createElement("tr");
+    let tableData1st = document.createElement("td");
+    let tableData2nd = document.createElement("td");
+    let tableData3rd = document.createElement("td");
+    let tableData4th = document.createElement("td");
 
+    tableData1st.textContent = this.foodID;
+    tableData2nd.textContent = this.foodName;
+    tableData3rd.textContent = this.type;
+    tableData4th.textContent = this.price;
 
-   function handleSubmit(event){
-    event.preventDefult();
-     let Id = creatId();
-     let FoodName = event.targert.FoodName.value;
-     let FoodType =  event.targert.FoodType.value;
-     let FoodPrice =  event.targert.FoodPrice.value;
-   
-
-     let newFood = new  foodObj (Id,FoodName,FoodType,FoodPrice);
-
-
-
-
-
-   }
-
-
-      
-   
-   Food.prototype.creatId = function(){
-    this.FoodID = math.floor(math.random()*(2000 - 1000 +1)) +1000;
- }
+    tableRow.appendChild(tableData1st);
+    tableRow.appendChild(tableData2nd);
+    tableRow.appendChild(tableData3rd);
+    tableRow.appendChild(tableData4th);
+    tablePlace.appendChild(tableRow);
+}
 
 
- Food.prototype.table1 = function(){
+let newFood = document.getElementById("formID");
+newFood.addEventListener("submit",doSubmit);
+function doSubmit(event) {
+event.preventDefault();
+let foodName = event.target.foodNameID.value;  
+let type = event.target.foodTypeID.value;
+let price = event.target.priceID.value;
 
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-    
-    table.appendChild(thead);
-    table.appendChild(tbody); 
-  
+let newFoodOnMenu = new Food (foodName,type,price);
+newFoodOnMenu.render();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
- }
+}
